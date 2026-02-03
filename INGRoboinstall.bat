f13af5f080184a1e58cca6d820b8e2cfb4707c1c
Ingranalerts <alerts@ingran.co.za>
2026-02-03 11:33:21 +0200

    Update INGRoboinstall.bat

--- INGRoboinstall.bat
+++ INGRoboinstall.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\INGRAN\INGRoboscript.ps1"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\INGRAN\Roboscript.ps1"
 exit /b %errorlevel%
